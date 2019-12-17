7da0b2d6dbe260a610a0832f2611b6a019d4a85e
Nicolas Chiaruttini <nicolas.chiaruttini@epfl.ch>
2019-12-17 11:01:18 +0100

    adds BigWarp action and Command - removes unnecessary GetSourceAndConverter by index

---
+++ src/main/java/bdv/util/ViewerPanelHandle.java
@@ -0,0 +1,68 @@
+package bdv.util;
+
+import bdv.tools.brightness.ConverterSetup;
+import bdv.tools.transformation.ManualTransformationEditor;
+import bdv.viewer.SourceAndConverter;
+import bdv.viewer.ViewerPanel;
+import bigwarp.BigWarp;
+import org.scijava.ui.behaviour.util.InputActionBindings;
+import org.scijava.ui.behaviour.util.TriggerBehaviourBindings;
+
+import java.util.List;
+import java.util.function.Consumer;
+
+/**
+ * Wraps a BigDataViewer instance into a BdvHandle
+ * This class NEEDS to be in bdv.util or it cannot implement the createViewer method
+ */
+
+public class ViewerPanelHandle extends BdvHandle {
+
+    Consumer<String> errlog = s -> System.err.println(this.getClass()+" error: "+s);
+
+    public String name;
+
+    public ViewerPanelHandle(ViewerPanel viewerPanel, String name) {
+        super(BdvOptions.options());
+        this.viewer = viewerPanel;
+        this.name = name;
+    }
+
+    @Override
+    public void close() {
+        // TODO : implement this ?
+    }
+
+    @Override
+    public ManualTransformationEditor getManualTransformEditor() {
+        errlog.accept("Unsupported getManualTransformEditor call in ViewerPanel wrapped BdvHandle");
+        return null;
+    }
+
+    @Override
+    public InputActionBindings getKeybindings() {
+        errlog.accept("Unsupported getKeybindings call in ViewerPanel wrapped BdvHandle");
+        return null;
+    }
+
+    @Override
+    public TriggerBehaviourBindings getTriggerbindings() {
+        System.err.println("Unsupported getTriggerbindings call in ViewerPanel wrapped BdvHandle");
+        return null;
+    }
+
+    boolean createViewer(
+            List< ? extends ConverterSetup> converterSetups,
+            List< ? extends SourceAndConverter< ? >> sources,
+            int numTimepoints ) {
+        errlog.accept("Cannot add sources in ViewerPanel wrapped BdvHandle BdvHandle");
+        return false;
+    }
+
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+}
