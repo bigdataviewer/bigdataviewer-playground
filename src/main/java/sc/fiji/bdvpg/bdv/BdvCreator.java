989fad118ad69ff230c43bbf51198f37372a9306
Christian Tischer <christian.tischer@embl.de>
2019-12-18 19:02:29 +0100

    Refactor as discussed

---
+++ src/main/java/sc/fiji/bdvpg/bdv/BdvCreator.java
@@ -0,0 +1,63 @@
+package sc.fiji.bdvpg.bdv;
+
+import bdv.util.BdvFunctions;
+import bdv.util.BdvHandle;
+import bdv.util.BdvOptions;
+import bdv.util.BdvStackSource;
+import bdv.viewer.Source;
+import net.imglib2.img.array.ArrayImg;
+import net.imglib2.img.array.ArrayImgs;
+import net.imglib2.realtransform.AffineTransform3D;
+
+public class BdvCreator implements Runnable
+{
+	private final boolean is2D;
+	private final String name;
+	private BdvHandle bdvHandle;
+
+	public BdvCreator( boolean is2D )
+	{
+		this( is2D, "BigDataViewer" );
+	}
+
+	public BdvCreator( boolean is2D, String name )
+	{
+		this.is2D = is2D;
+		this.name = name;
+	}
+
+
+	@Override
+	public void run()
+	{
+		createEmptyBdv();
+	}
+
+
+	/**
+	 * Hack: add an image and remove it after the
+	 * bdvHandle has been created.
+	 */
+	private void createEmptyBdv()
+	{
+		BdvOptions bdvOptions = BdvOptions.options();
+		if (is2D)
+			bdvOptions = bdvOptions.is2D();
+
+		ArrayImg dummyImg = ArrayImgs.bytes(2, 2, 2);
+
+		BdvStackSource bss = BdvFunctions.show(
+				dummyImg, "dummy",
+				bdvOptions.frameTitle(name)
+						.sourceTransform(new AffineTransform3D()));
+
+		bdvHandle = bss.getBdvHandle();
+
+		bss.removeFromBdv();
+	}
+
+	public BdvHandle getBdvHandle()
+	{
+		return bdvHandle;
+	}
+}
---
+++ src/test/src/sc/fiji/bdvpg/bdv/BdvCreatorDemo.java
@@ -0,0 +1,13 @@
+package sc.fiji.bdvpg.bdv;
+
+import bdv.util.BdvHandle;
+
+public class BdvCreatorDemo
+{
+	public static void main( String[] args )
+	{
+		final BdvCreator bdvCreator = new BdvCreator( false, "Hello World!" );
+		bdvCreator.run();
+		final BdvHandle bdvHandle = bdvCreator.getBdvHandle();
+	}
+}
