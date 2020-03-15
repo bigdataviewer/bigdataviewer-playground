f4ffdec7b0048d4705e3f7d9ac88c51b6af14a96
Nicolas Chiaruttini <nicolas.chiaruttini@epfl.ch>
2020-03-15 15:25:09 +0100

    adds documentation NewSourceCommand

--- src/main/java/sc/fiji/bdvpg/scijava/command/source/NewSourceCommand.java
+++ src/main/java/sc/fiji/bdvpg/scijava/command/source/NewSourceCommand.java
@@ -15,22 +15,34 @@ import sc.fiji.bdvpg.sourceandconverter.importer.NewSourceAndConverterGetter;
 
 import static net.imglib2.cache.img.DiskCachedCellImgOptions.options;
 
+/**
+ * Command which creates an empty Source based on a model Source
+ * The created source will cover the same portion of space as the model source,
+ * but with the specified voxel size, and at a specific timepoint
+ */
+
 @Plugin(type = Command.class, menuPath = ScijavaBdvDefaults.RootMenu+"Sources>New Source Based on Model Source")
 public class NewSourceCommand implements Command {
     
-    @Parameter
+    @Parameter(label = "Model Source", description = "Defines the portion of space covered by the new source")
     SourceAndConverter model;
 
-    @Parameter
+    @Parameter(label = "Source name")
     String name;
 
     @Parameter(type = ItemIO.OUTPUT)
     SourceAndConverter newsource;
     
-    @Parameter
-    double voxSizeX, voxSizeY, voxSizeZ;
+    @Parameter(label = "Voxel Size X")
+    double voxSizeX;
 
-    @Parameter
+    @Parameter(label = "Voxel Size Y")
+    double voxSizeY;
+
+    @Parameter(label = "Voxel Size Z")
+    double voxSizeZ;
+
+    @Parameter(label = "Timepoint (0 based index)")
     int timePoint;
 
     @Override
@@ -47,74 +59,6 @@ public class NewSourceCommand implements Command {
 
         // Creates cached image factory of Type UnsignedShort
         final DiskCachedCellImgFactory<UnsignedShortType> factory = new DiskCachedCellImgFactory<>( new UnsignedShortType(), factoryOptions );
-
-        /*RandomAccessibleInterval rai = model.getSpimSource().getSource(timePoint,0);
-
-        long nPixModelX = rai.dimension(0);
-        long nPixModelY = rai.dimension(1);
-        long nPixModelZ = rai.dimension(2);
-
-        AffineTransform3D at3Dorigin = new AffineTransform3D();
-
-        model.getSpimSource().getSourceTransform(timePoint,0,at3Dorigin);
-
-        // Origin
-        double[] x0 = new double[3];
-        at3Dorigin.apply(new double[]{0,0,0}, x0);
-
-        // xMax
-        double[] pt = new double[3];
-        double dist;
-
-        at3Dorigin.apply(new double[]{1,0,0},pt);
-
-        dist = (pt[0]-x0[0])*(pt[0]-x0[0]) +
-                      (pt[1]-x0[1])*(pt[1]-x0[1]) +
-                      (pt[2]-x0[2])*(pt[2]-x0[2]);
-
-        double distx =  Math.sqrt(dist);
-
-        dist = Math.sqrt(dist)*nPixModelX;
-
-        double nPixX = dist/voxSizeX;
-
-        at3Dorigin.apply(new double[]{0,1,0},pt);
-
-        dist = (pt[0]-x0[0])*(pt[0]-x0[0]) +
-                (pt[1]-x0[1])*(pt[1]-x0[1]) +
-                (pt[2]-x0[2])*(pt[2]-x0[2]);
-
-        double disty =  Math.sqrt(dist);
-
-        dist = Math.sqrt(dist)*nPixModelY;
-        double nPixY = dist/voxSizeY;
-
-        at3Dorigin.apply(new double[]{0,0,1},pt);
-
-        dist = (pt[0]-x0[0])*(pt[0]-x0[0]) +
-                (pt[1]-x0[1])*(pt[1]-x0[1]) +
-                (pt[2]-x0[2])*(pt[2]-x0[2]);
-
-        double distz =  Math.sqrt(dist);
-
-        dist = Math.sqrt(dist)*nPixModelZ;
-        double nPixZ = dist/voxSizeZ;
-
-        double[] m = at3Dorigin.getRowPackedCopy();
-
-        m[0] = m[0]/distx * voxSizeX;
-        m[4] = m[4]/distx * voxSizeX;
-        m[8] = m[8]/distx * voxSizeX;
-
-        m[1] = m[1]/disty * voxSizeY;
-        m[5] = m[5]/disty * voxSizeY;
-        m[9] = m[9]/disty * voxSizeY;
-
-        m[2] = m[2]/distz * voxSizeZ;
-        m[6] = m[6]/distz * voxSizeZ;
-        m[10] = m[10]/distz * voxSizeZ;
-
-        at3Dorigin.set(m);*/
 
         newsource = new NewSourceAndConverterGetter(name, model, timePoint, voxSizeX, voxSizeY, voxSizeZ, factory).get();
     }
