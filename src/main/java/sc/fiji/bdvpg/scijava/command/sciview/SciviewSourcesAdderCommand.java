fc01a1d6cda8f4965476b29bc00950cc92e15b17
Kyle IS Harrington <kyle.i.s.harrington@gmail.com>
2020-04-16 17:00:29 -0400

    Update versions of sciview, scenery, kotlin, joml. Add main to SciviewSourcesAdderCommand

--- src/main/java/sc/fiji/bdvpg/scijava/command/sciview/SciviewSourcesAdderCommand.java
+++ src/main/java/sc/fiji/bdvpg/scijava/command/sciview/SciviewSourcesAdderCommand.java
@@ -1,11 +1,14 @@
 package sc.fiji.bdvpg.scijava.command.sciview;
 
 import bdv.viewer.SourceAndConverter;
+import net.imagej.ImageJ;
 import org.scijava.command.Command;
 import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
 import sc.fiji.bdvpg.scijava.ScijavaBdvDefaults;
 import sc.iview.SciView;
+
+import java.util.HashMap;
 
 @Plugin(type = Command.class, menuPath = ScijavaBdvDefaults.RootMenu+"SciView>Add Sources")
 public class SciviewSourcesAdderCommand implements Command {
@@ -27,5 +30,20 @@ public class SciviewSourcesAdderCommand implements Command {
         for (SourceAndConverter sac : sacs) {
             sciView.addVolume(sac.getSpimSource().getSource(timePoint, mipmapLevel), sac.getSpimSource().getName(), "");
         }
+    }
+
+    static public void main(String... args) {
+        SciView sciview = null;
+        try {
+            sciview = SciView.createSciView();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        ImageJ imagej = new ImageJ(sciview.getScijavaContext());
+
+        HashMap<String, Object> argmap = new HashMap<>();
+
+        imagej.command().run(SciviewSourcesAdderCommand.class, true, argmap);
     }
 }
