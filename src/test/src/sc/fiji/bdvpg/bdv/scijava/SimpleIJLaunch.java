6add9290e3e6fa8336da370999658ddb9781cd37
Nicolas Chiaruttini <nicolas.chiaruttini@epfl.ch>
2020-04-17 13:01:03 +0200

    adds sciview context in SimpleIJLaunch class

--- src/test/src/sc/fiji/bdvpg/bdv/scijava/SimpleIJLaunch.java
+++ src/test/src/sc/fiji/bdvpg/bdv/scijava/SimpleIJLaunch.java
@@ -1,17 +1,24 @@
 package sc.fiji.bdvpg.bdv.scijava;
 
-import bdv.util.BdvHandle;
 import net.imagej.ImageJ;
+import sc.iview.SciView;
 
 public class SimpleIJLaunch {
 
     static public void main(String... args) {
+
+        SciView sciview = null;
+        try {
+            sciview = SciView.createSciView();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        ImageJ imagej = new ImageJ(sciview.getScijavaContext());
         // Arrange
         // create the ImageJ application context with all available services
-        final ImageJ ij = new ImageJ();
-        ij.ui().showUI();
-
-        BdvHandle bdvh;
+        //final ImageJ ij = new ImageJ();
+        imagej.ui().showUI();
 
     }
 }
