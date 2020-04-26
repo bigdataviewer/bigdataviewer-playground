2a70f0a8a1ede3624578cfda49a2ad1c7c04a978
Nicolas Chiaruttini <nicolas.chiaruttini@epfl.ch>
2020-04-26 16:15:53 +0200

    adds snapshot dependencies + catch error if converter from string to bdvhandle[] fails

--- src/main/java/sc/fiji/bdvpg/scijava/converters/StringToBdvHandleArray.java
+++ src/main/java/sc/fiji/bdvpg/scijava/converters/StringToBdvHandleArray.java
@@ -4,6 +4,7 @@ import bdv.util.BdvHandle;
 import com.google.gson.Gson;
 import org.scijava.convert.AbstractConverter;
 import org.scijava.convert.ConvertService;
+import org.scijava.log.LogService;
 import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
 import java.util.stream.Stream;
@@ -14,11 +15,26 @@ public class StringToBdvHandleArray extends AbstractConverter<String, BdvHandle[
     @Parameter
     ConvertService cs;
 
+    @Parameter
+    LogService ls;
+
     @Override
     public <T> T convert(Object src, Class<T> dest) {
         Gson gson = new Gson();
-        String[] bdvhsNames = gson.fromJson((String) src, String[].class);
-        return (T) Stream.of(bdvhsNames).map(name -> cs.convert(name, BdvHandle.class)).toArray(BdvHandle[]::new);
+        try {
+            String[] bdvhsNames = gson.fromJson((String) src, String[].class);
+            return (T) Stream.of(bdvhsNames).map(name -> cs.convert(name, BdvHandle.class)).toArray(BdvHandle[]::new);
+        } catch (Exception e1) {
+            try {
+                // Because IJ1 unboxing removes brackets. Just redo an attempt with brackets
+                String[] bdvhsNames = gson.fromJson("["+src+"]", String[].class);
+                return (T) Stream.of(bdvhsNames).map(name -> cs.convert(name, BdvHandle.class)).toArray(BdvHandle[]::new);
+            } catch (Exception e2) {
+                // For debugging
+                // ls.error("Could not convert "+src+" to an array of BdvHandle");
+                return null;
+            }
+        }
     }
 
     @Override
