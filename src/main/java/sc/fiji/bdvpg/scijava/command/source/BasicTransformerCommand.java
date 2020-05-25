d5eddd8fa0cdbc8d0baa216590b35e8ee4161676
Nicolas Chiaruttini <nicolas.chiaruttini@epfl.ch>
2020-05-25 15:36:57 +0200

    adds local basic transformations

--- src/main/java/sc/fiji/bdvpg/scijava/command/source/BasicTransformerCommand.java
+++ src/main/java/sc/fiji/bdvpg/scijava/command/source/BasicTransformerCommand.java
@@ -1,6 +1,7 @@
 package sc.fiji.bdvpg.scijava.command.source;
 
 import bdv.viewer.SourceAndConverter;
+import net.imglib2.RealPoint;
 import net.imglib2.realtransform.AffineTransform3D;
 import net.imglib2.ui.TransformEventHandler3D;
 import net.imglib2.util.LinAlgHelpers;
@@ -39,40 +40,55 @@ public class BasicTransformerCommand implements Command {
     @Override
     public void run() {
         for (SourceAndConverter sac : sources_in) {
-            if (!globalChange) {
-                throw new UnsupportedOperationException("TODO - contact the authors of bigdataviewer-playground in github!");
-            } else {
-                AffineTransform3D at3D = new AffineTransform3D();
-                at3D.identity();
+            {
+                AffineTransform3D at3D_global = new AffineTransform3D();
+                at3D_global.identity();
                 switch (type) {
-                    case "Flip": flip(at3D );
+                    case "Flip": flip(at3D_global);
                     break;
-                    case "Rot90": rot(1, at3D );
+                    case "Rot90": rot(1, at3D_global );
                     break;
-                    case "Rot180": rot(2, at3D );
+                    case "Rot180": rot(2, at3D_global );
                     break;
-                    case "Rot270": rot(3, at3D );
+                    case "Rot270": rot(3, at3D_global );
                     break;
                 }
-                SourceTransformHelper.append(at3D, new SourceAndConverterAndTimeRange(sac, timepoint));
+                if (globalChange) {
+                    SourceTransformHelper.append(at3D_global, new SourceAndConverterAndTimeRange(sac, timepoint));
+                } else {
+                    // Maintain center of box constant
+                    AffineTransform3D at3D = new AffineTransform3D();
+                    at3D.identity();
+                    //double[] m = at3D.getRowPackedCopy();
+                    sac.getSpimSource().getSourceTransform(timepoint,0,at3D);
+                    long[] dims = new long[3];
+                    sac.getSpimSource().getSource(timepoint,0).dimensions(dims);
+
+                    RealPoint ptCenterGlobalBefore = new RealPoint(3);
+                    RealPoint ptCenterPixel = new RealPoint((dims[0]-1.0)/2.0,(dims[1]-1.0)/2.0, (dims[2]-1.0)/2.0);
+
+                    at3D.apply(ptCenterPixel, ptCenterGlobalBefore);
+
+
+                    RealPoint ptCenterGlobalAfter = new RealPoint(3);
+
+                    at3D_global.apply(ptCenterGlobalBefore, ptCenterGlobalAfter);
+
+                    // Just shifting
+                    double[] m = at3D_global.getRowPackedCopy();
+
+                    m[3] -= ptCenterGlobalAfter.getDoublePosition(0)-ptCenterGlobalBefore.getDoublePosition(0);
+
+                    m[7] -= ptCenterGlobalAfter.getDoublePosition(1)-ptCenterGlobalBefore.getDoublePosition(1);
+
+                    m[11] -= ptCenterGlobalAfter.getDoublePosition(2)-ptCenterGlobalBefore.getDoublePosition(2);
+
+                    at3D_global.set(m);
+
+                    SourceTransformHelper.append(at3D_global, new SourceAndConverterAndTimeRange(sac, timepoint));
+
+                }
             }
-            /*
-            // Get affine transform
-            AffineTransform3D at3D = new AffineTransform3D();
-            sac.getSpimSource().getSourceTransform(timepoint,0,at3D);
-            // Get dimension
-            long[] dims = new long[3];
-            sac.getSpimSource().getSource(timepoint,0).dimensions(dims);
-            AffineTransform3D at3DInverse = at3D.inverse().copy();
-
-            AffineTransform3D transform3D = new AffineTransform3D();
-
-
-
-            // Apply : append
-            AffineTransform3D at3DOut = new AffineTransform3D();
-            SourceAndConverterUtils.append(at3DOut, sac);
-            */
         }
         SourceAndConverterServices.getSourceAndConverterDisplayService()
                 .updateDisplays(sources_in);
@@ -92,7 +108,6 @@ public class BasicTransformerCommand implements Command {
         }
     }
 
-    //
     private void rot(int quarterTurn, AffineTransform3D at3D) {
         switch (axis) {
             case "X":
