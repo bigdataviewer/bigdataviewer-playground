f5e03ff4016e432e74c7b031346dde6565dff5cf
Nicolas Chiaruttini <nicolas.chiaruttini@epfl.ch>
2020-09-07 23:03:47 +0200

    adds support of color and min max in serialization

--- src/main/java/sc/fiji/bdvpg/services/serializers/SourceAndConverterAdapter.java
+++ src/main/java/sc/fiji/bdvpg/services/serializers/SourceAndConverterAdapter.java
@@ -12,7 +12,11 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParseException;
 import com.google.gson.JsonSerializationContext;
 import com.google.gson.JsonSerializer;
+import net.imglib2.display.ColorConverter;
+import net.imglib2.type.numeric.ARGBType;
 import sc.fiji.bdvpg.services.SourceAndConverterSerializer;
+import sc.fiji.bdvpg.services.SourceAndConverterServices;
+import sc.fiji.bdvpg.sourceandconverter.display.ColorChanger;
 
 import java.lang.reflect.Type;
 
@@ -33,7 +37,21 @@ public class SourceAndConverterAdapter implements JsonSerializer<SourceAndConver
         obj.addProperty("source name", sourceAndConverter.getSpimSource().getName());
         obj.addProperty("source class", sourceAndConverter.getSpimSource().getClass().getName());
         obj.addProperty("converter class", sourceAndConverter.getConverter().getClass().toString());
-        obj.add("converter", jsonSerializationContext.serialize(sourceAndConverter.getConverter()));
+        //obj.add("converter", jsonSerializationContext.serialize(sourceAndConverter.getConverter()));
+
+        if (sourceAndConverter.getConverter() instanceof ColorConverter) {
+            ColorConverter colorConverter = (ColorConverter) sourceAndConverter.getConverter();
+            obj.add("color", jsonSerializationContext.serialize(colorConverter.getColor().get()));
+            double min = SourceAndConverterServices.getSourceAndConverterDisplayService()
+                    .getConverterSetup(sourceAndConverter).getDisplayRangeMin();
+            double max = SourceAndConverterServices.getSourceAndConverterDisplayService()
+                    .getConverterSetup(sourceAndConverter).getDisplayRangeMax();
+            obj.addProperty("converter_setup_min", min);
+            obj.addProperty("converter_setup_max", max);
+        }
+
+
+
         //obj.addProperty("source id", sacToId.get(sourceAndConverter));
         JsonElement element = serializeSubClass(sourceAndConverter, SourceAndConverter.class, jsonSerializationContext);
         obj.add("sac", element);
@@ -69,16 +87,29 @@ public class SourceAndConverterAdapter implements JsonSerializer<SourceAndConver
     @Override
     public SourceAndConverter deserialize(JsonElement jsonElement, Type type, JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
         JsonObject jsonObject = jsonElement.getAsJsonObject();
-        System.out.println("Bouh!");
 
         String sourceClass = jsonObject.getAsJsonPrimitive("source class").getAsString();
 
-        System.out.println(sourceClass);
-        System.out.println(SpimSource.class.getName());
+        SourceAndConverter sac = null;
 
         if (sourceClass.equals(SpimSource.class.getName())) {
-            System.out.println("SpimSource!");
-            return SpimSourceAndConverterAdapter.deserialize(jsonObject.get("sac"), SourceAndConverter.class, jsonDeserializationContext);
+            sac = SpimSourceAndConverterAdapter.deserialize(jsonObject.get("sac"), SourceAndConverter.class, jsonDeserializationContext);
+        }
+
+        if (sac != null) {
+            // Now the color
+            if (jsonObject.getAsJsonPrimitive("color")!=null) {
+                int color = jsonObject.getAsJsonPrimitive("color").getAsInt();
+                new ColorChanger(sac,  new ARGBType(color)).run(); // TO deal with volatile and non volatile
+
+                SourceAndConverterServices.getSourceAndConverterDisplayService()
+                        .getConverterSetup(sac).setDisplayRange(
+                                jsonObject.getAsJsonPrimitive("converter_setup_min").getAsDouble(),
+                                jsonObject.getAsJsonPrimitive("converter_setup_max").getAsDouble());
+
+            }
+            // Min Max display
+            return sac;
         }
 
         return null;
