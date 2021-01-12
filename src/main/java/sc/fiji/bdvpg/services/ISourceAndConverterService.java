6645e6b72191f74a281cefd6ffba00eaa31bc8c4
Nicolas Chiaruttini <nicolas.chiaruttini@epfl.ch>
2021-01-12 21:30:33 +0100

    license update

--- src/main/java/sc/fiji/bdvpg/services/ISourceAndConverterService.java
+++ src/main/java/sc/fiji/bdvpg/services/ISourceAndConverterService.java
@@ -2,7 +2,7 @@
  * #%L
  * BigDataViewer-Playground
  * %%
- * Copyright (C) 2019 - 2020 Nicolas Chiaruttini, EPFL - Robert Haase, MPI CBG - Christian Tischer, EMBL
+ * Copyright (C) 2019 - 2021 Nicolas Chiaruttini, EPFL - Robert Haase, MPI CBG - Christian Tischer, EMBL
  * %%
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
