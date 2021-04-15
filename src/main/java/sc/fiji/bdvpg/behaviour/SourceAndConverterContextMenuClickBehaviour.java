4afd542499e2673b831fbc4b19330532bdf43786
Nicolas Chiaruttini <nicolas.chiaruttini@epfl.ch>
2021-04-16 00:06:58 +0200

    shows context menu even if no source is selected

--- src/main/java/sc/fiji/bdvpg/behaviour/SourceAndConverterContextMenuClickBehaviour.java
+++ src/main/java/sc/fiji/bdvpg/behaviour/SourceAndConverterContextMenuClickBehaviour.java
@@ -95,10 +95,10 @@ public class SourceAndConverterContextMenuClickBehaviour implements ClickBehavio
 
 		final List<SourceAndConverter> sacs = new ArrayList<>(sourcesSupplier.get());
 
-		if ( sacs.size() == 0 )
-			return;
+		//if ( sacs.size() == 0 )
+		//	return;
 
-		String message = "";
+		/*String message = "";
 		if (sacs.size()>1) {
 			message +=  sacs.size()+" sources selected";
 		} else {
@@ -107,7 +107,7 @@ public class SourceAndConverterContextMenuClickBehaviour implements ClickBehavio
 			}
 		}
 
-		bdv.getViewerPanel().showMessage(message);
+		bdv.getViewerPanel().showMessage(message);*/
 
 		final SourceAndConverterPopupMenu popupMenu = new SourceAndConverterPopupMenu( () -> sacs.toArray(new SourceAndConverter[0]), popupActions );
 
