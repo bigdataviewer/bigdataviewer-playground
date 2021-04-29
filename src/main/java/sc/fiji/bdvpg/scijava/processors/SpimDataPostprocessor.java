d59b58f6291c5dab6b8f537784bc6611f70e72aa
Nicolas Chiaruttini <nicolas.chiaruttini@epfl.ch>
2021-04-29 17:35:14 +0200

    renames postprocessors with more appropriate names

--- src/main/java/sc/fiji/bdvpg/scijava/processors/SpimDataPostprocessor.java
+++ src/main/java/sc/fiji/bdvpg/scijava/processors/SpimDataPostprocessor.java
@@ -28,7 +28,7 @@
  */
 package sc.fiji.bdvpg.scijava.processors;
 
-import bdv.viewer.SourceAndConverter;
+import mpicbg.spim.data.generic.AbstractSpimData;
 import org.scijava.module.Module;
 import org.scijava.module.process.AbstractPostprocessorPlugin;
 import org.scijava.module.process.PostprocessorPlugin;
@@ -39,44 +39,34 @@ import sc.fiji.bdvpg.scijava.services.SourceAndConverterService;
 import java.util.function.Consumer;
 
 @Plugin(type = PostprocessorPlugin.class)
-public class BdvSourceAndConverterPostprocessor extends AbstractPostprocessorPlugin {
+public class SpimDataPostprocessor extends AbstractPostprocessorPlugin {
 
     @Parameter
-    SourceAndConverterService bss;
+	SourceAndConverterService bss;
 
-    public static Consumer<String> log = (str) -> {};//System.out.println(BdvSourceAndConverterPostprocessor.class.getSimpleName()+":"+str);
+    public static Consumer<String> log = (str) -> System.out.println( SpimDataPostprocessor.class.getSimpleName()+":"+str);
+
+    public static Consumer<String> errlog = (str) -> System.err.println( SpimDataPostprocessor.class.getSimpleName()+":"+str);
 
     @Override
     public void process(Module module) {
 
        module.getOutputs().forEach((name, object)-> {
            //log.accept("input:\t"+name+"\tclass:\t"+object.getClass().getSimpleName());
-           if (object instanceof SourceAndConverter) {
-               SourceAndConverter sac = (SourceAndConverter) object;
-               log.accept("Source found.");
-               log.accept("Is it registered ? ");
-               if (!bss.isRegistered(sac)) {
-                   log.accept("No.");
-                   bss.register(sac);
-               } else {
-                   log.accept("Yes.");
-               }
+           if (object instanceof AbstractSpimData) {
+               AbstractSpimData asd = (AbstractSpimData) object;
+               log.accept("SpimData found.");
+               bss.register(asd);
                module.resolveOutput(name);
            }
-           if (object instanceof SourceAndConverter[]) {
-               SourceAndConverter[] sacs = (SourceAndConverter[]) object;
-               for (SourceAndConverter sac:sacs) {
-                   log.accept("Source found.");
-                   log.accept("Is it registered ? ");
-                   if (!bss.isRegistered(sac)) {
-                       log.accept("No.");
-                       bss.register(sac);
-                   } else {
-                       log.accept("Yes.");
-                   }
+           if (object instanceof AbstractSpimData[]) {
+               AbstractSpimData[] asds = (AbstractSpimData[]) object;
+               for (AbstractSpimData asd:asds) {
+                   log.accept( "SpimData found." );
+                   bss.register( asd );
+                   module.resolveOutput( name );
                }
-               module.resolveOutput(name);
-           }
+            }
        });
     }
 }
