1de4dd0721b02766e776b4b3d2ddcfedc8dbb270
Christian Tischer <christian.tischer@embl.de>
2021-06-17 16:46:51 +0200

    Remove NotNull and add getWindowCentreInCalibratedUnits

--- src/main/java/sc/fiji/bdvpg/bdv/BdvHandleHelper.java
+++ src/main/java/sc/fiji/bdvpg/bdv/BdvHandleHelper.java
@@ -35,7 +35,6 @@ import net.imglib2.*;
 import net.imglib2.realtransform.AffineTransform3D;
 import net.imglib2.util.Intervals;
 import net.imglib2.util.LinAlgHelpers;
-import org.jetbrains.annotations.NotNull;
 import org.scijava.cache.CacheService;
 import org.scijava.object.ObjectService;
 import org.scijava.ui.behaviour.io.InputTriggerConfig;
@@ -98,20 +97,29 @@ public class BdvHandleHelper
 
         adaptedViewerTransform.translate( targetPositionInViewerInPixels );
 
-        final double[] windowCentreInViewerInPixels = getWindowCentreInPixels( bdvHandle );
+        final double[] windowCentreInViewerInPixels = getWindowCentreInPixelUnits( bdvHandle );
 
         adaptedViewerTransform.translate( windowCentreInViewerInPixels );
 
         return adaptedViewerTransform;
     }
 
-    @NotNull
-    private static double[] getWindowCentreInPixels( BdvHandle bdvHandle )
+    public static double[] getWindowCentreInPixelUnits( BdvHandle bdvHandle )
     {
-        final double[] windowCentreInViewerInPixels = new double[ 3 ];
-        windowCentreInViewerInPixels[ 0 ] = bdvHandle.getViewerPanel().getDisplay().getWidth() / 2.0;
-        windowCentreInViewerInPixels[ 1 ] = bdvHandle.getViewerPanel().getDisplay().getHeight() / 2.0;
-        return windowCentreInViewerInPixels;
+        final double[] windowCentreInPixelUnits = new double[ 3 ];
+        windowCentreInPixelUnits[ 0 ] = bdvHandle.getViewerPanel().getDisplay().getWidth() / 2.0;
+        windowCentreInPixelUnits[ 1 ] = bdvHandle.getViewerPanel().getDisplay().getHeight() / 2.0;
+        return windowCentreInPixelUnits;
+    }
+
+    public static double[] getWindowCentreInCalibratedUnits( BdvHandle bdvHandle )
+    {
+        final double[] centreInPixelUnits = getWindowCentreInPixelUnits( bdvHandle );
+        final AffineTransform3D affineTransform3D = new AffineTransform3D();
+        bdvHandle.getViewerPanel().state().getViewerTransform( affineTransform3D );
+        final double[] centreInCalibratedUnits = new double[ 3 ];
+        affineTransform3D.inverse().apply( centreInPixelUnits, centreInCalibratedUnits );
+        return centreInCalibratedUnits;
     }
 
     public static double getViewerVoxelSpacing( BdvHandle bdv ) {
