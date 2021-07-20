507f8c6a26aef3b85c35546fc97f36a93e47182f
Nicolas Chiaruttini <nicolas.chiaruttini@epfl.ch>
2021-07-20 17:49:58 +0200

    now correctly select sources event if the node does not display the sources explicitely

--- src/main/java/sc/fiji/persist/ScijavaGsonHelper.java
+++ src/main/java/sc/fiji/persist/ScijavaGsonHelper.java
@@ -34,7 +34,6 @@ import org.scijava.Context;
 import org.scijava.InstantiableException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import sc.fiji.bdvpg.scijava.services.ui.SourceAndConverterServiceUI;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -137,8 +136,6 @@ public class ScijavaGsonHelper {
                         e.printStackTrace();
                     }
                 });
-
-
 
         return builder.setPrettyPrinting();
     }
