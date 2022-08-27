26a1e67c109708d032a0237e4f6fdc8d0b82c8da
Nicolas Chiaruttini <nicolas.chiaruttini@epfl.ch>
2022-08-27 20:07:37 +0200

    format and updates file header and update bdv-selector

--- src/main/java/sc/fiji/bdvpg/cache/GlobalLoaderCache.java
+++ src/main/java/sc/fiji/bdvpg/cache/GlobalLoaderCache.java
@@ -1,13 +1,8 @@
 /*-
  * #%L
- * ImgLib2: a general-purpose, multidimensional image processing library.
+ * BigDataViewer-Playground
  * %%
- * Copyright (C) 2017 - 2020 Tobias Pietzsch, Stephan Preibisch, Stephan Saalfeld,
- * John Bogovic, Albert Cardona, Barry DeZonia, Christian Dietz, Jan Funke,
- * Aivar Grislis, Jonathan Hale, Grant Harris, Stefan Helfrich, Mark Hiner,
- * Martin Horn, Steffen Jaensch, Lee Kamentsky, Larry Lindsey, Melissa Linkert,
- * Mark Longair, Brian Northan, Nick Perry, Curtis Rueden, Johannes Schindelin,
- * Jean-Yves Tinevez and Michael Zinsmaier.
+ * Copyright (C) 2019 - 2022 Nicolas Chiaruttini, EPFL - Robert Haase, MPI CBG - Christian Tischer, EMBL
  * %%
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
