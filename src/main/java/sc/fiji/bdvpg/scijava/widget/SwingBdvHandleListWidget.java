83309ba3178580a4563fba325ec823ffc507f74a
Nicolas Chiaruttini <nicolas.chiaruttini@epfl.ch>
2026-01-05 13:29:29 +0100

    update licenses

--- src/main/java/sc/fiji/bdvpg/scijava/widget/SwingBdvHandleListWidget.java
+++ src/main/java/sc/fiji/bdvpg/scijava/widget/SwingBdvHandleListWidget.java
@@ -2,7 +2,7 @@
  * #%L
  * BigDataViewer-Playground
  * %%
- * Copyright (C) 2019 - 2025 Nicolas Chiaruttini, EPFL - Robert Haase, MPI CBG - Christian Tischer, EMBL
+ * Copyright (C) 2019 - 2026 Nicolas Chiaruttini, EPFL - Robert Haase, MPI CBG - Christian Tischer, EMBL
  * %%
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
